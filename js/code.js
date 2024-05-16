1e061b2373e1d58b3c903e2db4fc54faa3ea5ef1
Franciszek Gotkowski <129520490+Paprykojad@users.noreply.github.com>
2024-05-16 17:50:50 +0200

    Add files via upload

--- js/code.js
+++ js/code.js
@@ -13,16 +13,31 @@ function genmem() {
 
 function genzart() {
     let x = document.createElement('p')
-    let y = document.createTextNode('uwr;oiuwqoiruwoeirumnxv.,mnxcva, asdjf;lk kjkjkjkjkj')
+
+    let y = document.createTextNode('Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.')
+
     x.appendChild(y)
-    let z = document.createElement('div')
     let n = document.createAttribute('class')
     n.value = 'p'
-    z.appendChild(n)
-    z.appendChild(x)
-    return z
+    x.setAttributeNode(n)
+    return x
 }
 
 function genmysl() {
+    let x = document.createElement('input')
+    x.type = 'text'
+    x.id = 'mysl'
+    x.name = 'mysl'
 
+    let y = document.createElement('label')
+    y.htmlFor = 'mysl'
+    y.appendChild(document.createTextNode('Myśl roku'))
+    let w = document.createElement('div')
+    let b = document.createElement('input')
+    b.type = 'button'
+    b.onclick = null
+    w.appendChild(y)
+    w.appendChild(x)
+    w.appendChild(b)
+    return w
 }
